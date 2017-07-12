979590f1fa1d0dbe28d49cad4d1ac1ce63a75256
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-12 16:24:33 +0100

    done core work

--- db/console.rb
+++ db/console.rb
@@ -8,3 +8,18 @@ album1 = Album.new({'title' => 'Ruby Nightmares', 'genre' => "Spooky Sublime", '
 
 
 album1.save
+
+Album.all
+Artist.all
+
+p artist1.albums
+p album1.artist
+
+artist1.name = "Codeclan Codependent"
+
+artist1.update
+p artist1
+
+album1.title = "Ruby Dreams"
+album1.update
+p album1
